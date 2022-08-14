b80ad78837f45341d46c078e9db4f06c48de6a09
Antonio Prado <antonioeprado18@gmail.com>
2022-08-13 21:15:31 -0300

    Finalizado

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,7 +1,18 @@
 let check = false;
 let count = 0;
+let nome, endereco;
+
 const modal = document.querySelector("#modal");
+const pratoModal = document.querySelector(".prato");
+const bebidaModal = document.querySelector(".bebida");
+const sobremesaModal = document.querySelector(".sobremesa");
+const precoPratoModal = document.querySelector(".preco-prato");
+const precoBebidaModal = document.querySelector(".preco-bebida");
+const precoSobremesaModal = document.querySelector(".preco-sobremesa");
+const precoTotalModal = document.querySelector(".preco-total");
+const submitLink = document.querySelector("a");
 const cancelButton = document.querySelector(".cancel");
+const submitButton = document.querySelector(".submit");
 const cardSelector = document.querySelectorAll(".card");
 const cardList = [];
 const button = document.querySelector("button");
@@ -10,30 +21,43 @@ button.setAttribute("disabled", "disabled");
 cardSelector.forEach((element) => element.addEventListener("click", event => checkButton(event)));
 
 function checkoutCart() {
-    
-    const pratoModal = document.querySelector(".prato");
-    const bebidaModal = document.querySelector(".bebida");
-    const sobremesaModal = document.querySelector(".sobremesa");
-    const precoPratoModal = document.querySelector(".preco-prato");
-    const precoBebidaModal = document.querySelector(".preco-bebida");
-    const precoSobremesaModal = document.querySelector(".preco-sobremesa");
-    const precoTotalModal = document.querySelector(".preco-total");
+
     let prato, bebida, sobremesa, preco;
+
+    nome = prompt("Digite seu nome: ");
+    endereco = prompt("Digite seu endereço: ");    
     
     cardList.forEach((arguments) => {
         if(arguments["container-pratos"]) prato = arguments["container-pratos"].children;
         if(arguments["container-bebidas"]) bebida = arguments["container-bebidas"].children;
         if(arguments["container-sobremesas"]) sobremesa = arguments["container-sobremesas"].children;
     });
+
     pratoModal.innerText = prato.item(1).innerText;
     precoPratoModal.innerText = prato.item(3).innerText;
     bebidaModal.innerText = bebida.item(1).innerText;
     precoBebidaModal.innerText = bebida.item(3).innerText;
     sobremesaModal.innerText = sobremesa.item(1).innerText;
     precoSobremesaModal.innerText = sobremesa.item(3).innerText;
+
     preco = Number((precoPratoModal.innerText.slice(3)).replace(",", ".")) + Number((precoBebidaModal.innerText.slice(3)).replace(",", ".")) + Number((precoSobremesaModal.innerText.slice(3)).replace(",", "."));
     precoTotalModal.innerText = `R$ ${String(preco.toFixed(2)).replace(".", ",")}`;
+
     modal.style.display = "flex";
+
+    if(nome, endereco !== null) {
+        const mensagem = `Olá, gostaria de fazer o pedido:
+        - Prato: ${pratoModal.innerText}
+        - Bebida: ${bebidaModal.innerText}
+        - Sobremesa: ${sobremesaModal.innerText}
+        Total: ${precoTotalModal.innerText}
+        
+        Nome: ${nome}
+        Endereço: ${endereco}`;
+        const url = `https://wa.me/5532988770112?text=${encodeURIComponent(mensagem)}`;
+        console.log(url)
+        submitLink.setAttribute("href", url);
+    }
 }
 
 
@@ -51,11 +75,13 @@ function updateCheckoutButton(count) {
         button.style.backgroundColor = "#32B72F";
         button.addEventListener("click", () => checkoutCart());
         cancelButton.addEventListener("click", () => modal.style.display = "none")
+        
     }
 }
 
 
 function checkButton(e) {
+
     const sectionSelector = (e.currentTarget.parentNode).querySelectorAll(".card");
     const section = e.currentTarget.parentNode.parentNode.className;
     const cardSelected = e.currentTarget;  
@@ -80,3 +106,4 @@ function checkButton(e) {
         updateCheckoutButton(count);
     }
 }
+
